7e14b91d6db23ede145e981d7b613bcd7eb62150
Brayden039 <113641758+Brayden039@users.noreply.github.com>
2023-02-25 05:30:45 +0000

    finished

--- Main.java
+++ Main.java
@@ -2,6 +2,26 @@ class Main
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello world!");
+    randomMatrix randMat = new randomMatrix(3, 4, 10);
+    randMat.matrixPrint();
+    System.out.println("\n" + "Get Amount:");
+    System.out.println(randMat.getAmount(0));
+    System.out.println("\n" + "Get Total:");
+    System.out.println(randMat.getTotal());
+    System.out.println("\n" + "Get Rows Total:");
+    int[] rowarray = randMat.getrowTotals();
+    for(int rows: rowarray)
+    {
+      System.out.println(rows + " ");
+    }
+    System.out.println("\n" + "Get Columns Total:");
+    int[] colarray = randMat.getcolumnTotals();
+    for(int cols: colarray)
+    {
+      System.out.println(cols + " ");
+    }
+    System.out.println("\n" + "All values Unique:");
+    System.out.println(randMat.allvaluesUnique());
+    
   }
 }
--- randomMatrix.java
+++ randomMatrix.java
@@ -20,16 +20,120 @@ public class randomMatrix
 
   public void matrixPrint()
   {
-    for(int i2 = 0; i2 < matrix.length; i2++)
+    for(int i = 0; i < matrix.length; i++)
     {
-      for(int a2 = 0; a2 < matrix[i2].length; a2++)
+      for(int a = 0; a < matrix[i].length; a++)
       {
-        System.out.println(matrix[i2][a2]);
+        System.out.print(matrix[i][a]);
           
       }
+      System.out.println();
     }
       
   }
+
+  int count2 = 0;
+  public int getAmount(int num1)
+  {
+    for(int i = 0; i < matrix.length; i++)
+    {
+      for(int a = 0; a < matrix[i].length; a++)
+      {
+        if(num1 == matrix[i][a])
+        {
+          count2++;
+        }
+      }
+    }
+    return(count2);
+      
+  }
+
+  public int[] getrowTotals()
+  {
+    int[] rowTotals = new int[matrix.length];
+    for(int i = 0; i < matrix.length; i++)
+    {
+      int count4 = 0;
+      for(int j = 0; j < matrix[i].length; j++)
+      {
+       count4 = count4 + matrix[i][j];
+      }
+      rowTotals[i] = count4;
+      
+        
+    }
+    return(rowTotals);
+   
+    
+  }
+  
+
+  public int[] getcolumnTotals()
+  {
+    int[] colTotals = new int[matrix[0].length];
+    for(int a = 0; a < matrix[0].length; a++)
+    {
+      int count5 = 0;
+      for(int i = 0; i < matrix.length; i++)
+      {
+        count5 = count5 + matrix[i][a];
+          
+      }
+      colTotals[a] = count5;
+    }
+    return(colTotals);
+  
+  }
+  
+
+  int total2 = 0;
+  public int getTotal()
+  {
+    for(int i = 0; i < matrix.length; i++)
+    {
+      for(int a = 0; a < matrix[i].length; a++)
+      {
+        total2 = total2 + matrix[i][a];
+          
+      }
+    }
+    return(total2);
+  }
+
+  
+  
+  public boolean allvaluesUnique()
+  {
+    for(int i = 0; i < matrix.length; i++)
+    {
+      for(int a = 0; a < matrix[i].length; a++)
+      {
+        int count3 = 0;
+        for(int k = 0; k < matrix.length; k++)
+        {
+          for(int l = 0; l < matrix[k].length; l++)
+          {
+            if(matrix[i][a] == matrix[k][l])
+            {
+              count3++;
+            }
+          }
+          if(count3 > 1)
+          {
+            return false;
+          }
+        }
+       }
+    }
+    return true;
+  }
+  
+  
+  
+    
     
     
 }
+    
+
